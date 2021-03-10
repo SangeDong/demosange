4c5f93a0282db012648cb8c66364490a48f89c22
sangedon <sange.dong@outlook.com>
2021-03-10 13:28:12 +0800

    mybatis simple implementation

--- sabatis/batis/src/main/java/com/sangedon/batis/resource/ResourceReader.java
+++
@@ -1,9 +0,0 @@
-package com.sangedon.batis.resource;
-
-import java.io.InputStream;
-
-public final class ResourceReader {
-    public static InputStream load(String path) {
-        return ResourceReader.class.getClassLoader().getResourceAsStream(path);
-    }
-}
--- sabatis/batis/src/main/java/com/sangedon/batis/session/SqlSessionFactory.java
+++
@@ -1,5 +0,0 @@
-package com.sangedon.batis.session;
-
-public interface SqlSessionFactory {
-    public SqlSession openSession();
-}
